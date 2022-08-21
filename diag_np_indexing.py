01fc29177b861c2b9116b9741308b3ab83e85054
aaanX11 <aaa1n1istp7r89w@gmail.com>
2022-08-21 15:40:23 +0300

    cleaned and usage example added

--- diag_np_indexing.py
+++ diag_np_indexing.py
@@ -1,8 +1,10 @@
 import numpy as np
 
-def diag_index(img):
-    n = 320
-    arr = np.arange(n ** 2).reshape((n, n))
+def indexer(n):
+    arr = np.empty((n, n, 2), dtype='int32')
+    arr[:, :, 0] = np.arange(n)[:, None]
+    arr[:, :, 1] = np.arange(n)[None, :]
+
     diag = [[] for _ in range(2 * n - 1)]
     for i in range(n - 1):
         diag[i] = [arr[i - j, j] for j in range(i + 1)]
@@ -11,13 +13,30 @@ def diag_index(img):
     for i in range(n - 1):
         diag[n + i] = [arr[n - 1 - j, 1 + i + j] for j in range(n - i - 1)]
 
-    rows, cols = img.shape
+    return diag
 
+def find_outermost_pts_in_rot45(arr):
+    rows, cols = arr.shape
+    assert rows == cols, 'Square arrays only for now'
+
+    n = rows
+    diag = indexer(n)
+
+    diag_bb = {}
     for id, d in enumerate(diag):
-        t = img.ravel()[d]
-        for idx, el in enumerate(img.ravel()[d]):
-            print(el)
-        print()
+        # for el in arr[tuple(np.asarray(d).T)]:
+        for ix, iy in d:
+            el = arr[ix, iy]
+            if not el in diag_bb:
+                diag_bb[el]= [[ix,iy]]*4
+            else:
+                diag_bb[el][1] = [ix, iy]
+                if iy - ix > diag_bb[el][2][1] - diag_bb[el][2][0]:
+                    diag_bb[el][2] = [ix, iy]
+                if iy - ix < diag_bb[el][3][1] - diag_bb[el][3][0]:
+                    diag_bb[el][3] = [ix, iy]
+
+    return diag_bb
 
 
 if __name__ == '__main__':
